a28d2a6cd6de5387efd78146fec280b309ab0d7a
Danil <my@ro2005.ru>
2023-07-02 23:37:25 +0700

    update configs

---
+++ config/main-example.php
@@ -0,0 +1,6 @@
+<?php
+return [
+	'debug' => true,
+	'coreLogFileName' => 'core',
+	'coreLogDetail' => false
+];
---
+++ config/mysql-example.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'mysqlLogFileName' => 'mysql',
+	'mysqlLogDetail' => false,
+
+	'mysqlHost' => 'ro2005.ru',
+	'mysqlDatabase' => 'database_name',
+	'mysqlUsername' => 'danilo9',
+	'mysqlPassword' => 'my_password'
+];
