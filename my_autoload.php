fedfdcff33334bf3a9030e684c2c9e72fdad34a7
danilo9 <kalinindanilq7734@gmail.com>
2023-09-06 18:04:56 +0700

    Update autoload

--- my_autoload.php
+++ my_autoload.php
@@ -5,9 +5,9 @@ require_once 'src/Abstract/AbstractMigration.php';
 require_once 'src/Abstract/AbstractModel.php';
 require_once 'src/Abstract/AbstractException.php';
 
-require_once 'src/Controllers/HomeController.php';
-require_once 'src/Controllers/AboutController.php';
-require_once 'src/Controllers/ContactController.php';
+foreach (glob('../src/Controllers/*.php') as $file) {
+    require_once $file;
+}
 
 require_once 'src/HttpRequest/HttpServer.php';
 require_once 'src/HttpRequest/HttpGetParams.php';
@@ -16,9 +16,9 @@ require_once 'src/HttpRequest/HttpPostParams.php';
 require_once 'src/HttpResponse/HttpResponseBody.php';
 require_once 'src/HttpResponse/HttpResponseJson.php';
 
-require_once 'src/Exceptions/ForbiddenException.php';
-require_once 'src/Exceptions/InternalServerErrorException.php';
-require_once 'src/Exceptions/PageNotFoundException.php';
+foreach (glob('../src/Exceptions/*.php') as $file) {
+    require_once $file;
+}
 
 require_once 'src/Logger.php';
 require_once 'src/SystemVar.php';
@@ -28,7 +28,9 @@ require_once 'src/HttpRequest.php';
 require_once 'src/HttpResponse.php';
 require_once 'src/DB.php';
 
-require_once 'src/Models/User.php';
+foreach (glob('../src/Models/*.php') as $file) {
+    require_once $file;
+}
 
 require_once 'src/DKCore.php';
 require_once 'src/Router.php';
